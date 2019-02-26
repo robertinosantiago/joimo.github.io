e549322f284f5ff31a62147f5e2aa205ffdbafdb
Gabriel A <33150863+Joimo@users.noreply.github.com>
2019-02-26 08:58:04 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//SIDENAV
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+
+  });
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+  $(document).ready(function(){
+      $('.slider').slider();
+    });
+
+    // Menu-toggle button
+    $(document).ready(function() {
+          $(".menu-icon").on("click", function() {
+                $("nav ul").toggleClass("showing");
+          });
+    });
+
+    // Scrolling Effect
+
+    $(window).on("scroll", function() {
+          if($(window).scrollTop() > 50 ) {
+                $('nav').addClass('black');
+          }
+          else {
+                $('nav').removeClass('black');
+
+          }
+    })
+
+    //Modal
+    $(document).ready(function(){
+    $('.modal').modal();
+  });
